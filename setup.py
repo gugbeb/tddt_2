3bd5fde3537b26ccb140b2da5003171e9c9b7dce
Igor Krivenko <igor.s.krivenko@gmail.com>
2023-02-17 15:53:25 +0100

    Bump version to 0.2.0 and add SciPy as a dependency

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='tddt',
-    version='0.1.0',
+    version='0.2.0',
     url='https://github.com/krivenko/tddt.git',
     author='Viktor Valmispild, Igor Krivenko',
     author_email='valmispild@gmail.com, igor.s.krivenko@gmail.com',
     description='Implementation of the time-dependent dual TRILEX theory',
     packages=['tddt'],
-    install_requires=['numpy >= 1.12.0'],
+    install_requires=['numpy >= 1.12.0', 'scipy >= 1.7.0'],
 )
