109b3e42c4496741317b58c346b67af19f107929
Igor Krivenko <igor.s.krivenko@gmail.com>
2023-04-20 18:09:07 +0200

    retime: New free functions `conv_ret_l()` and `conv_l_adv()`
    These functions compute a real time convolution of a retarded/lesser
    or lesser/advanced pair of GF's.

--- tddt/retime.py
+++ tddt/retime.py
@@ -1,10 +1,33 @@
 #
-# Auxiliary routines for Green's functions of real time
+# Auxiliary routines for 2-point Green's functions of real time
 #
 
 import numpy as np
 
 from triqs.gf import Gf, MeshReTime, MeshProduct
+
+from .util import subscripts
+from .integration import GregoryIntegrator
+
+
+def _is_2t_gf(g: Gf) -> bool:
+    """Is g a 2-point real time GF?"""
+    return len(g.mesh.components) >= 2 and \
+        isinstance(g.mesh.components[0], MeshReTime) and \
+        isinstance(g.mesh.components[1], MeshReTime)
+
+
+def _extract_nli_nri(g: Gf, n_left_indices, assert_msg):
+    """Return numbers of left and right indices in a real time GF."""
+    if n_left_indices is None:
+        assert len(g.target_shape) % 2 == 0, assert_msg
+        nli = len(g.target_shape) // 2
+    else:
+        nli = n_left_indices
+
+    nri = len(g.target_shape) - nli
+
+    return nli, nri
 
 
 def conj(g: Gf, *, n_left_indices=None) -> Gf:
@@ -17,24 +40,19 @@ def conj(g: Gf, *, n_left_indices=None) -> Gf:
     n_left_indices: Number of axes in G's target shape corresponding to the
                     multi-index 'a'. By default, a half of all axes.
     """
-
-    assert len(g.mesh.components) >= 2
-    assert isinstance(g.mesh.components[0], MeshReTime)
-    assert isinstance(g.mesh.components[1], MeshReTime)
+    assert _is_2t_gf(g)
 
     mesh = MeshProduct(g.mesh.components[1],
                        g.mesh.components[0],
                        *g.mesh.components[2:])
 
-    if n_left_indices is None:
-        assert len(g.target_shape) % 2 == 0, \
-            "n_left_indices must be provided when the target shape of the GF " \
-            "has an odd number of dimensions"
-        nli = len(g.target_shape) // 2
-    else:
-        nli = n_left_indices
+    nli, nri = _extract_nli_nri(
+        g,
+        n_left_indices,
+        "n_left_indices must be provided when the target shape of the GF "
+        "has an odd number of dimensions"
+    )
 
-    nri = len(g.target_shape) - nli
     ts = g.target_shape[nli:] + g.target_shape[:nli]
 
     g_conj = Gf(mesh=mesh, target_shape=ts)
@@ -43,3 +61,162 @@ def conj(g: Gf, *, n_left_indices=None) -> Gf:
     g_conj.data[:] = np.conj(np.moveaxis(g.data, axes_from, axes_to))
 
     return g_conj
+
+
+def _make_target_shape_subs(a_nli, a_nri, b_nli, b_nri):
+    tgs = subscripts['target']
+    subs_a_tg = tgs[:a_nli + a_nri]
+    subs_b_tg = tgs[a_nli:(a_nli + b_nli + b_nri)]
+    subs_res_tg = tgs[:a_nli]
+    subs_res_tg += tgs[(a_nli + a_nri):(a_nli + b_nli + b_nri)]
+    return subs_a_tg, subs_b_tg, subs_res_tg
+
+
+def conv_ret_l(a_ret: Gf,
+               b_l: Gf,
+               a_ret_n_left_indices=None,
+               gregory_order=5) -> Gf:
+    r"""
+    Compute a real-time convolution of an extended retarded function and a
+    lesser function,
+
+    F_{a, b}(t, t') =
+        \sum_c \int_0^t d\bar t A^r_{a, c}(t, \bar t) B^<_{c, b}(\bar t, t').
+
+    a_ret: The extended retarded function A^r(t, t').
+    b_l: The lesser function B^<(t, t').
+    a_ret_n_left_indices: Number of axes in A^r's target shape corresponding to
+                          the multi-index 'a'. By default, a half of all axes.
+    gregory_order: Order of the Gregory quadrature rule used to do the
+                   convolution.
+    """
+    assert _is_2t_gf(a_ret)
+    assert _is_2t_gf(b_l)
+
+    assert a_ret.mesh.components[1] == b_l.mesh.components[0], \
+           "Incompatible time meshes of a_ret and b_l"
+    assert a_ret.mesh.components[2:] == b_l.mesh.components[2:], \
+           "Different non-time meshes of a_ret and b_l"
+
+    mesh = MeshProduct(a_ret.mesh.components[0],
+                       b_l.mesh.components[1],
+                       *a_ret.mesh.components[2:])
+
+    a_ret_nli, a_ret_nri = _extract_nli_nri(
+        a_ret,
+        a_ret_n_left_indices,
+        "a_ret_n_left_indices must be provided when the target shape of A^r "
+        "has an odd number of dimensions"
+    )
+
+    b_l_nli = a_ret_nri
+    b_l_nri = len(b_l.target_shape) - b_l_nli
+
+    assert a_ret.target_shape[a_ret_nli:] == b_l.target_shape[:b_l_nli], \
+        "Incompatible target shapes of a_ret and b_l"
+
+    # Generate einsum() subscripts
+
+    ts = subscripts['time']
+    subs_a_ret_t = ts[0] + ts[2]
+    subs_b_l_t = ts[2] + ts[1]
+    subs_res_t = ts[0] + ts[1]
+    subs_w = subs_a_ret_t
+
+    subs_nt = subscripts['nontime'][:len(a_ret.mesh.components[2:])]
+
+    subs_a_ret_tg, subs_b_l_tg, subs_res_tg = \
+        _make_target_shape_subs(a_ret_nli, a_ret_nri, b_l_nli, b_l_nri)
+
+    target_shape_res = \
+        a_ret.target_shape[:a_ret_nli] + b_l.target_shape[b_l_nli:]
+
+    # Perform summation
+
+    subs_a_ret = subs_a_ret_t + subs_nt + subs_a_ret_tg
+    subs_b_l = subs_b_l_t + subs_nt + subs_b_l_tg
+    subs_res = subs_res_t + subs_nt + subs_res_tg
+
+    subs = f"{subs_a_ret},{subs_w},{subs_b_l}->{subs_res}"
+
+    res = Gf(mesh=mesh, target_shape=target_shape_res)
+
+    w = GregoryIntegrator(gregory_order).weights(a_ret.mesh.components[1])
+    res.data[:] = np.einsum(subs, a_ret.data, w, b_l.data, optimize="optimal")
+
+    return res
+
+
+def conv_l_adv(a_l: Gf,
+               b_adv: Gf,
+               a_l_n_left_indices=None,
+               gregory_order=5) -> Gf:
+    r"""
+    Compute a real-time convolution of a lesser function and an extended
+    advanced function,
+
+    F_{a, b}(t, t') =
+        \sum_c \int_0^{t'} d\bar t A^<_{a, c}(t, \bar t) B^a{c, b}(\bar t, t').
+
+    a_l: The lesser function A^<(t, t').
+    b_l: The extended advanced function B^a(t, t').
+    a_l_n_left_indices: Number of axes in A^<'s target shape corresponding to
+                        the multi-index 'a'. By default, a half of all axes.
+    gregory_order: Order of the Gregory quadrature rule used to do the
+                   convolution.
+    """
+    assert _is_2t_gf(a_l)
+    assert _is_2t_gf(b_adv)
+
+    assert a_l.mesh.components[1] == b_adv.mesh.components[0], \
+           "Incompatible time meshes of a_l and b_adv"
+    assert a_l.mesh.components[2:] == b_adv.mesh.components[2:], \
+           "Different non-time meshes of a_l and b_adv"
+
+    mesh = MeshProduct(a_l.mesh.components[0],
+                       b_adv.mesh.components[1],
+                       *a_l.mesh.components[2:])
+
+    a_l_nli, a_l_nri = _extract_nli_nri(
+        a_l,
+        a_l_n_left_indices,
+        "a_l_n_left_indices must be provided when the target shape of A^< "
+        "has an odd number of dimensions"
+    )
+
+    b_adv_nli = a_l_nri
+    b_adv_nri = len(b_adv.target_shape) - b_adv_nli
+
+    assert a_l.target_shape[a_l_nli:] == b_adv.target_shape[:b_adv_nli], \
+        "Incompatible target shapes of a_l and b_adv"
+
+    # Generate einsum() subscripts
+
+    ts = subscripts['time']
+    subs_a_l_t = ts[0] + ts[2]
+    subs_b_adv_t = ts[2] + ts[1]
+    subs_res_t = ts[0] + ts[1]
+    subs_w = ts[1] + ts[2]
+
+    subs_nt = subscripts['nontime'][:len(a_l.mesh.components[2:])]
+
+    subs_a_l_tg, subs_b_adv_tg, subs_res_tg = \
+        _make_target_shape_subs(a_l_nli, a_l_nri, b_adv_nli, b_adv_nri)
+
+    target_shape_res = \
+        a_l.target_shape[:a_l_nli] + b_adv.target_shape[b_adv_nli:]
+
+    # Perform summation
+
+    subs_a_l = subs_a_l_t + subs_nt + subs_a_l_tg
+    subs_b_adv = subs_b_adv_t + subs_nt + subs_b_adv_tg
+    subs_res = subs_res_t + subs_nt + subs_res_tg
+
+    subs = f"{subs_a_l},{subs_w},{subs_b_adv}->{subs_res}"
+
+    res = Gf(mesh=mesh, target_shape=target_shape_res)
+
+    w = GregoryIntegrator(gregory_order).weights(a_l.mesh.components[1])
+    res.data[:] = np.einsum(subs, a_l.data, w, b_adv.data, optimize="optimal")
+
+    return res
--- tests/test_retime.py
+++ tests/test_retime.py
@@ -1,12 +1,18 @@
 import unittest
-from numpy.testing import assert_array_equal, assert_array_almost_equal
+from itertools import product
 import numpy as np
 
 from triqs.gf import MeshReTime, MeshBrillouinZone, MeshProduct, Gf
 from triqs.lattice import BravaisLattice, BrillouinZone
+from triqs.utility.comparison_tests import assert_gfs_are_close
 
-from tddt.retime import conj
-from tddt.keldysh import from_lesser_greater, retarded, advanced
+from tddt.retime import conj, conv_ret_l, conv_l_adv
+from tddt.keldysh import (from_lesser_greater,
+                          retarded,
+                          retarded_ext,
+                          advanced,
+                          advanced_ext)
+from tddt.integration import GregoryIntegrator
 
 
 class test_retime(unittest.TestCase):
@@ -19,24 +25,22 @@ class test_retime(unittest.TestCase):
         cls.t_mesh = MeshReTime(0, cls.t_max, cls.n_t)
 
         cls.bl = BravaisLattice(units=[(1, 0, 0)])  # Square lattice
+        cls.n_k = 5
+        cls.bz_mesh = MeshBrillouinZone(BrillouinZone(cls.bl), cls.n_k)
+        cls.eps_k = np.array([k.value[0] / np.pi + 1.0 for k in cls.bz_mesh])
 
-    def test_conj(self):
+    def _make_g_l_g_g_scalar(self, x=1.0):
         mesh = MeshProduct(self.t_mesh, self.t_mesh)
-
-        # Scalar-valued GF
         g_l = Gf(mesh=mesh, target_shape=())
         g_g = Gf(mesh=mesh, target_shape=())
         for t1, t2 in mesh:
             e = np.exp(-1j * (t1.value - t2.value))
-            g_g[t1, t2] = -1j * (1.0 - 0.1) * e
-            g_l[t1, t2] = -1j * (-0.1) * e
-        g = from_lesser_greater(g_l, g_g)
+            g_g[t1, t2] = -1j * (1.0 - 0.1 * x) * e
+            g_l[t1, t2] = -1j * (-0.1 * x) * e
+        return g_l, g_g
 
-        g_adv = conj(retarded(g))
-        g_adv_ref = advanced(g)
-        assert_array_equal(g_adv.data, g_adv_ref.data)
-
-        # Matrix-valued GF
+    def _make_g_l_g_g_matrix(self, x=1.0):
+        mesh = MeshProduct(self.t_mesh, self.t_mesh)
         g_l = Gf(mesh=mesh, target_shape=(3, 3))
         g_g = Gf(mesh=mesh, target_shape=(3, 3))
         H = np.array([[1.0, 0.5, -0.1j],
@@ -45,38 +49,144 @@ class test_retime(unittest.TestCase):
         E, U = np.linalg.eig(H)
         for t1, t2 in MeshProduct(self.t_mesh, self.t_mesh):
             dt = (t1.value - t2.value)
-            e = U @ np.diag(np.exp(-1j * E * dt)) @ np.conj(U.T)
-            g_g[t1, t2] = -1j * (1.0 - 0.1) * e
-            g_l[t1, t2] = -1j * (-0.1) * e
-        g = from_lesser_greater(g_l, g_g)
+            e = U @ np.diag(np.exp(-1j * x * E * dt)) @ np.conj(U.T)
+            g_g[t1, t2] = -1j * (1.0 - 0.1 * x) * e
+            g_l[t1, t2] = -1j * (-0.1 * x) * e
+        return g_l, g_g
 
-        g_adv = conj(retarded(g))
-        g_adv_ref = advanced(g)
-        assert_array_almost_equal(g_adv.data, g_adv_ref.data, decimal=12)
+    def _make_g_l_g_g_matrix_bz(self, x=1.0):
+        mesh = MeshProduct(self.t_mesh, self.t_mesh, self.bz_mesh)
 
-        n_k = 3
-        bz_mesh = MeshBrillouinZone(BrillouinZone(self.bl), n_k)
-        eps_k = np.array([k.value[0] / np.pi + 1.0 for k in bz_mesh])
-        mesh = MeshProduct(self.t_mesh, self.t_mesh, bz_mesh)
-
-        # Matrix-valued GF with an extra mesh component
         g_l = Gf(mesh=mesh, target_shape=(3, 3))
         g_g = Gf(mesh=mesh, target_shape=(3, 3))
-        for k, eps in zip(bz_mesh, eps_k):
+        for k, eps in zip(self.bz_mesh, self.eps_k):
             H = np.array([[eps, 0.5, -0.1j],
                           [0.5, 2.0 * eps, 0.5],
                           [0.1j, 0.5, 3.0 * eps]])
             E, U = np.linalg.eig(H)
             for t1, t2 in MeshProduct(self.t_mesh, self.t_mesh):
                 dt = (t1.value - t2.value)
-                e = U @ np.diag(np.exp(-1j * E * dt)) @ np.conj(U.T)
-                g_g[t1, t2, k] = -1j * (1.0 - 0.1) * e
-                g_l[t1, t2, k] = -1j * (-0.1) * e
+                e = U @ np.diag(np.exp(-1j * x * E * dt)) @ np.conj(U.T)
+                g_g[t1, t2, k] = -1j * (1.0 - 0.1 * x) * e
+                g_l[t1, t2, k] = -1j * (-0.1 * x) * e
+        return g_l, g_g
+
+    def test_conj(self):
+        # Scalar-valued GF
+        g_l, g_g = self._make_g_l_g_g_scalar()
         g = from_lesser_greater(g_l, g_g)
 
         g_adv = conj(retarded(g))
         g_adv_ref = advanced(g)
-        assert_array_almost_equal(g_adv.data, g_adv_ref.data, decimal=12)
+        assert_gfs_are_close(g_adv, g_adv_ref, precision=1e-12)
+
+        # Matrix-valued GF
+        g_l, g_g = self._make_g_l_g_g_matrix()
+        g = from_lesser_greater(g_l, g_g)
+
+        g_adv = conj(retarded(g))
+        g_adv_ref = advanced(g)
+        assert_gfs_are_close(g_adv, g_adv_ref, precision=1e-12)
+
+        # Matrix-valued GF with an extra mesh component
+        g_l, g_g = self._make_g_l_g_g_matrix_bz()
+        g = from_lesser_greater(g_l, g_g)
+
+        g_adv = conj(retarded(g))
+        g_adv_ref = advanced(g)
+        assert_gfs_are_close(g_adv, g_adv_ref, precision=1e-12)
+
+    def test_conv(self):
+        order = 5
+        w = GregoryIntegrator(order).weights(self.t_mesh)
+
+        # Scalar-valued GF
+        a_l, a_g = self._make_g_l_g_g_scalar(1.0)
+        b_l, b_g = self._make_g_l_g_g_scalar(2.0)
+        a = from_lesser_greater(a_l, a_g)
+        b = from_lesser_greater(b_l, b_g)
+
+        a_ret_ext = retarded_ext(a)
+        b_adv_ext = advanced_ext(b)
+
+        a_ret_b_l = conv_ret_l(a_ret_ext, b_l)
+        a_l_b_adv = conv_l_adv(a_l, b_adv_ext)
+
+        a_ret_b_l_ref = Gf(mesh=a_ret_b_l.mesh, target_shape=())
+        a_l_b_adv_ref = Gf(mesh=a_l_b_adv.mesh, target_shape=())
+        for n, m in product(range(self.n_t), range(self.n_t)):
+            a_ret_b_l_ref.data[n, m] = sum(
+                w[n, j] * a_ret_ext.data[n, j] * b_l.data[j, m]
+                for j in range((n if (n > order) else order) + 1)
+            )
+            a_l_b_adv_ref.data[n, m] = sum(
+                w[m, j] * a_l.data[n, j] * b_adv_ext.data[j, m]
+                for j in range((m if (m > order) else order) + 1)
+            )
+
+        assert_gfs_are_close(a_ret_b_l, a_ret_b_l_ref, precision=1e-12)
+        assert_gfs_are_close(a_l_b_adv, a_l_b_adv_ref, precision=1e-12)
+
+        # Matrix-valued GF
+        a_l, a_g = self._make_g_l_g_g_matrix(1.0)
+        b_l, b_g = self._make_g_l_g_g_matrix(2.0)
+        a = from_lesser_greater(a_l, a_g)
+        b = from_lesser_greater(b_l, b_g)
+
+        a_ret_ext = retarded_ext(a)
+        b_adv_ext = advanced_ext(b)
+
+        a_ret_b_l = conv_ret_l(a_ret_ext, b_l)
+        a_l_b_adv = conv_l_adv(a_l, b_adv_ext)
+
+        a_ret_b_l_ref = Gf(mesh=a_ret_b_l.mesh, target_shape=(3, 3))
+        a_l_b_adv_ref = Gf(mesh=a_l_b_adv.mesh, target_shape=(3, 3))
+        for n, m, a, b, c in product(range(self.n_t),
+                                     range(self.n_t),
+                                     *[range(3)] * 3):
+            a_ret_b_l_ref.data[n, m, a, b] += sum(
+                w[n, j] * a_ret_ext.data[n, j, a, c] * b_l.data[j, m, c, b]
+                for j in range((n if (n > order) else order) + 1)
+            )
+            a_l_b_adv_ref.data[n, m, a, b] += sum(
+                w[m, j] * a_l.data[n, j, a, c] * b_adv_ext.data[j, m, c, b]
+                for j in range((m if (m > order) else order) + 1)
+            )
+
+        assert_gfs_are_close(a_ret_b_l, a_ret_b_l_ref, precision=1e-12)
+        assert_gfs_are_close(a_l_b_adv, a_l_b_adv_ref, precision=1e-12)
+
+        # Matrix-valued GF with an extra mesh component
+        a_l, a_g = self._make_g_l_g_g_matrix_bz(1.0)
+        b_l, b_g = self._make_g_l_g_g_matrix_bz(2.0)
+        a = from_lesser_greater(a_l, a_g)
+        b = from_lesser_greater(b_l, b_g)
+
+        a_ret_ext = retarded_ext(a)
+        b_adv_ext = advanced_ext(b)
+
+        a_ret_b_l = conv_ret_l(a_ret_ext, b_l)
+        a_l_b_adv = conv_l_adv(a_l, b_adv_ext)
+
+        a_ret_b_l_ref = Gf(mesh=a_ret_b_l.mesh, target_shape=(3, 3))
+        a_l_b_adv_ref = Gf(mesh=a_l_b_adv.mesh, target_shape=(3, 3))
+        for n, m, K, a, b, c in product(range(self.n_t),
+                                        range(self.n_t),
+                                        range(self.n_k),
+                                        *[range(3)] * 3):
+            a_ret_b_l_ref.data[n, m, K, a, b] += sum(
+                w[n, j]
+                * a_ret_ext.data[n, j, K, a, c] * b_l.data[j, m, K, c, b]
+                for j in range((n if (n > order) else order) + 1)
+            )
+            a_l_b_adv_ref.data[n, m, K, a, b] += sum(
+                w[m, j]
+                * a_l.data[n, j, K, a, c] * b_adv_ext.data[j, m, K, c, b]
+                for j in range((m if (m > order) else order) + 1)
+            )
+
+        assert_gfs_are_close(a_ret_b_l, a_ret_b_l_ref, precision=1e-12)
+        assert_gfs_are_close(a_l_b_adv, a_l_b_adv_ref, precision=1e-12)
 
 
 if __name__ == '__main__':
