0abb3fd8c2eb10348d1ffb49b3d390b276b29b65
Igor Krivenko <igor.s.krivenko@gmail.com>
2023-04-28 21:55:43 +0200

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tddt',
-    version='0.2.0',
+    version='0.3.0',
     url='https://github.com/krivenko/tddt.git',
     author='Viktor Valmispild, Igor Krivenko',
     author_email='valmispild@gmail.com, igor.s.krivenko@gmail.com',
