gugbeb/tddt_2

commit da40bf2a72af636763b591ac73f95886aab94bc3

keldysh: New factory function `KeldyshGF.from_arg_index_gen()`

tddt/keldysh.py
@@ -4,8 +4,8 @@
 
 from enum import Enum
 from copy import deepcopy
-from itertools import product, takewhile, islice
-from typing import Tuple, Dict, Union, Sequence, Optional
+from itertools import product, takewhile, islice, chain
+from typing import Tuple, Dict, Union, Sequence, Optional, Callable
 import numpy as np
 
 from triqs.gf import Gf, MeshReTime, MeshPoint, MeshProduct
@@ -132,6 +132,39 @@ class KeldyshGF:
             [Gf(mesh=self.mesh, target_shape=self.target_shape)
              for _ in range(2 ** self.n_args)]
         ).reshape((2,) * self.n_args)
+
+    @classmethod
+    def from_arg_index_gen(cls,
+                           generator: Callable, *,
+                           mesh: Union[MeshReTime, MeshProduct],
+                           arg_index_shapes: Tuple[Tuple[int, ...], ...]
+                           ):
+        """
+        Construct a matrix/tensor-valued KeldyshGF object out of scalar-valued
+        components returned by a given generator function.
+
+        The generator function used to construct an N-point GF must accept N
+        arguments, each of which is a tuple of integer indices from the ranges
+        determined by the respective parts of `arg_index_shapes`. The generator
+        is expected to return a scalar-valued KeldyshGF object.
+        """
+        g = KeldyshGF(mesh=mesh, arg_index_shapes=arg_index_shapes)
+
+        for indices in product(*map(np.ndindex, arg_index_shapes)):
+            g_el = generator(*indices)
+            assert isinstance(g_el, KeldyshGF), \
+                "Generator must return an instance of KeldyshGF"
+            assert g.n_args == g_el.n_args, \
+                f"Expected a {g.n_args}-point GF from the generator " \
+                f"(got a {g_el.n_args}-point GF)"
+            assert g.mesh == g_el.mesh, \
+                "GF returned by the generator has a wrong mesh"
+
+            for g_comp, g_el_comp in zip(g.components.flat,
+                                         g_el.components.flat):
+                g_comp.data[..., *chain.from_iterable(indices)] = g_el_comp.data
+
+        return g
 
     def __getitem__(self, args):
         args_t = args if isinstance(args, tuple) else (args,)

tests/test_keldysh.py
@@ -352,6 +352,30 @@ class test_keldysh(unittest.TestCase):
 
             self._test_gf(g)
 
+    def test_from_arg_index_gen(self):
+        ttt_mesh = MeshProduct(MeshReTime(0, 6.0, 7),
+                               MeshReTime(0, 6.0, 8),
+                               MeshReTime(0, 6.0, 9))
+        arg_index_shapes = ((2, 3), (3, 2), (4,))
+
+        def generator(ind1, ind2, ind3):
+            g_el = KeldyshGF(mesh=ttt_mesh)
+            for br in product(Branch, repeat=3):
+                val = (ind1[0] + ind1[1]) * (ind2[0] - ind2[1]) * (ind3[0] + 3)
+                g_el[*br].data[:] = val
+            return g_el
+
+        g = KeldyshGF.from_arg_index_gen(generator,
+                                         mesh=ttt_mesh,
+                                         arg_index_shapes=arg_index_shapes)
+
+        index_ranges = product(*map(range, (2, 3, 3, 2, 4)))
+        for ind in index_ranges:
+            val = (ind[0] + ind[1]) * (ind[2] - ind[3]) * (ind[4] + 3)
+            for br in product(Branch, repeat=3):
+                assert_array_equal(g[*br].data[..., *ind],
+                                   val * np.ones((7, 8, 9)))
+
     def test_keldysh_gf_bz(self):
         tt_mesh = MeshProduct(MeshReTime(0, 6.0, 7), MeshReTime(0, 6.0, 8))
         n_k = 10

doc/example.ipynb
@@ -191,6 +191,44 @@
   },
   {
    "cell_type": "markdown",
+   "id": "33b302a3-b057-4bd9-8bbd-4eb9d269fd2e",
+   "metadata": {},
+   "source": [
+    "Building a matrix/tensor-valued contour function out of scalar-valued components using `KeldyshGF.from_arg_index_gen()`."
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 8,
+   "id": "416d8190-6897-41ea-91eb-6a76931406fa",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "tt_mesh = MeshProduct(t_mesh, t_mesh)\n",
+    "\n",
+    "arg_index_shapes=((2, 3), # 2 spin projections, 3 orbitals \n",
+    "                  (2, 3)) # 2 spin projections, 3 orbitals\n",
+    "\n",
+    "# Generator of scalar-valued elements\n",
+    "def generator(ind1, ind2):\n",
+    "    sigma1, orb1 = ind1\n",
+    "    sigma2, orb2 = ind2\n",
+    "\n",
+    "    # Return a zero scalar-valued KeldyshGF object in the spin-offdiagonal case\n",
+    "    if sigma1 != sigma2:\n",
+    "        return KeldyshGF(mesh=tt_mesh)\n",
+    "    else:\n",
+    "        # Otherwise build the corresponding scalar-valued element e.g. by calling an impurity solver\n",
+    "        g_el = KeldyshGF(mesh=tt_mesh)\n",
+    "        # Fill g_el ...\n",
+    "        return g_el\n",
+    "\n",
+    "# Build a GF that is diagonal in spin indices\n",
+    "G = KeldyshGF.from_arg_index_gen(generator, mesh=tt_mesh, arg_index_shapes=arg_index_shapes)"
+   ]
+  },
+  {
+   "cell_type": "markdown",
    "id": "de9a76e6-07ec-4a79-b00a-857da652a0b7",
    "metadata": {
     "tags": []
@@ -201,7 +239,7 @@
   },
   {
    "cell_type": "code",
-   "execution_count": 7,
+   "execution_count": 9,
    "id": "b8d47b5f-5813-4c7f-87da-ad9d893a1174",
    "metadata": {
     "tags": []
@@ -246,7 +284,7 @@
   },
   {
    "cell_type": "code",
-   "execution_count": 8,
+   "execution_count": 10,
    "id": "f0993e3e-a281-4d17-aee0-44ea84ce7850",
    "metadata": {
     "tags": []
@@ -286,7 +324,7 @@
   },
   {
    "cell_type": "code",
-   "execution_count": 9,
+   "execution_count": 11,
    "id": "4a1f3653-bbab-4dd0-a9b3-347d8ccb7de3",
    "metadata": {
     "tags": []
@@ -343,7 +381,7 @@
   },
   {
    "cell_type": "code",
-   "execution_count": 10,
+   "execution_count": 12,
    "id": "30a70498-d562-45ca-a004-1d8e31a1044e",
    "metadata": {
     "tags": []
@@ -389,7 +427,7 @@
   },
   {
    "cell_type": "code",
-   "execution_count": 11,
+   "execution_count": 13,
    "id": "76c35398-0780-4438-88a3-3863711a5f2b",
    "metadata": {
     "tags": []
@@ -440,7 +478,7 @@
   },
   {
    "cell_type": "code",
-   "execution_count": 12,
+   "execution_count": 14,
    "id": "a7a9adef-ee31-4fa0-be6a-057fa9a0901b",
    "metadata": {
     "tags": []
@@ -524,7 +562,7 @@
   },
   {
    "cell_type": "code",
-   "execution_count": 13,
+   "execution_count": 15,
    "id": "04016d2b-e305-4d94-ac91-d098a0d0fbb7",
    "metadata": {
     "tags": []
@@ -561,7 +599,7 @@
   },
   {
    "cell_type": "code",
-   "execution_count": 14,
+   "execution_count": 16,
    "id": "6fccfc38-d74c-421e-b484-88a88df129a0",
    "metadata": {
     "tags": []
@@ -571,10 +609,10 @@
      "name": "stdout",
      "output_type": "stream",
      "text": [
-      "<tddt.keldysh.KeldyshGF object at 0x7ff9c8155950>\n",
-      "<tddt.keldysh.KeldyshGF object at 0x7ff9c80d5b10>\n",
-      "<tddt.keldysh.KeldyshGF object at 0x7ff9c9688750>\n",
-      "<tddt.keldysh.KeldyshGF object at 0x7ff9c8130190>\n",
+      "<tddt.keldysh.KeldyshGF object at 0x7f75ab35ea50>\n",
+      "<tddt.keldysh.KeldyshGF object at 0x7f759452f950>\n",
+      "<tddt.keldysh.KeldyshGF object at 0x7f75ac657e50>\n",
+      "<tddt.keldysh.KeldyshGF object at 0x7f75ac80acd0>\n",
       "True\n",
       "False\n"
      ]
@@ -610,7 +648,7 @@
   },
   {
    "cell_type": "code",
-   "execution_count": 15,
+   "execution_count": 17,
    "id": "e812727c-7686-4757-9049-2d59419b263c",
    "metadata": {
     "tags": []
@@ -640,7 +678,7 @@
   },
   {
    "cell_type": "code",
-   "execution_count": 16,
+   "execution_count": 18,
    "id": "3a056426-4aa1-4457-b030-f6e1d3190b3d",
    "metadata": {
     "tags": []
@@ -650,7 +688,7 @@
      "name": "stdout",
      "output_type": "stream",
      "text": [
-      "<tddt.keldysh.KeldyshGF object at 0x7ff9c8141f50>\n"
+      "<tddt.keldysh.KeldyshGF object at 0x7f7594593c50>\n"
      ]
     }
    ],
@@ -697,7 +735,7 @@
   },
   {
    "cell_type": "code",
-   "execution_count": 17,
+   "execution_count": 19,
    "id": "9eb5fde9-9990-46b6-9b10-00ad0e6d4f38",
    "metadata": {
     "tags": []
@@ -751,7 +789,7 @@
   },
   {
    "cell_type": "code",
-   "execution_count": 18,
+   "execution_count": 20,
    "id": "e1996367-c3d2-45e0-9431-5d7db35d6feb",
    "metadata": {
     "tags": []
@@ -796,7 +834,7 @@
   },
   {
    "cell_type": "code",
-   "execution_count": 19,
+   "execution_count": 21,
    "id": "0b509fc1-c2bc-428d-a8ab-0daf65c74e66",
    "metadata": {
     "tags": []
@@ -852,7 +890,7 @@
   },
   {
    "cell_type": "code",
-   "execution_count": 20,
+   "execution_count": 22,
    "id": "399e0c60-97f7-4cf8-9160-f9c912b5e4a3",
    "metadata": {
     "tags": []
@@ -892,7 +930,7 @@
   },
   {
    "cell_type": "code",
-   "execution_count": 21,
+   "execution_count": 23,
    "id": "8402ac1e-d539-4721-9bd5-0fe539ef321a",
    "metadata": {
     "tags": []
@@ -947,7 +985,7 @@
   },
   {
    "cell_type": "code",
-   "execution_count": 22,
+   "execution_count": 24,
    "id": "562694f2-2092-4d1d-ae16-b5abf6797e9a",
    "metadata": {
     "tags": []

commit f84bbc36969bbee58ce88c39ba6899aa0c90619e

keldysh: Fix compatibility issues introduced in da40bf2

tddt/keldysh.py
@@ -162,7 +162,9 @@ class KeldyshGF:
 
             for g_comp, g_el_comp in zip(g.components.flat,
                                          g_el.components.flat):
-                g_comp.data[..., *chain.from_iterable(indices)] = g_el_comp.data
+                sl = (slice(None),) * len(mesh.components) + \
+                    tuple(chain.from_iterable(indices))
+                g_comp.data[sl] = g_el_comp.data
 
         return g
 

tests/test_keldysh.py
@@ -360,9 +360,9 @@ class test_keldysh(unittest.TestCase):
 
         def generator(ind1, ind2, ind3):
             g_el = KeldyshGF(mesh=ttt_mesh)
-            for br in product(Branch, repeat=3):
+            for br1, br2, br3 in product(Branch, repeat=3):
                 val = (ind1[0] + ind1[1]) * (ind2[0] - ind2[1]) * (ind3[0] + 3)
-                g_el[*br].data[:] = val
+                g_el[br1, br2, br3].data[:] = val
             return g_el
 
         g = KeldyshGF.from_arg_index_gen(generator,
@@ -370,11 +370,13 @@ class test_keldysh(unittest.TestCase):
                                          arg_index_shapes=arg_index_shapes)
 
         index_ranges = product(*map(range, (2, 3, 3, 2, 4)))
-        for ind in index_ranges:
-            val = (ind[0] + ind[1]) * (ind[2] - ind[3]) * (ind[4] + 3)
-            for br in product(Branch, repeat=3):
-                assert_array_equal(g[*br].data[..., *ind],
-                                   val * np.ones((7, 8, 9)))
+        for i in index_ranges:
+            val = (i[0] + i[1]) * (i[2] - i[3]) * (i[4] + 3)
+            for br1, br2, br3 in product(Branch, repeat=3):
+                assert_array_equal(
+                    g[br1, br2, br3].data[:, :, :,
+                                          i[0], i[1], i[2], i[3], i[4]],
+                    val * np.ones((7, 8, 9)))
 
     def test_keldysh_gf_bz(self):
         tt_mesh = MeshProduct(MeshReTime(0, 6.0, 7), MeshReTime(0, 6.0, 8))